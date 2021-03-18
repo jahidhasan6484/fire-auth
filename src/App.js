1be91c9154f9b6cfb545b3b03bd72fda88984c2b
jahidhasan6484 <jahidhasan6484@gmail.com>
2021-03-18 18:07:26 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
 
   const googleProvider = new firebase.auth.GoogleAuthProvider();
   const fbProvider = new firebase.auth.FacebookAuthProvider();
+  const ghProvider = new firebase.auth.GithubAuthProvider();
 
   const handleSignIn = () => {
     firebase.auth()
@@ -47,28 +48,36 @@ function App() {
     firebase.auth()
       .signInWithPopup(fbProvider)
       .then((result) => {
-        /** @type {firebase.auth.OAuthCredential} */
         var credential = result.credential;
-
-        // The signed-in user info.
         var user = result.user;
-
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
         var accessToken = credential.accessToken;
-
-        // ...
         console.log('fb user: ', user);
       })
       .catch((error) => {
-        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        // The email of the user's account used.
         var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential);
+      });
+  }
 
-        // ...
+  const handleGithubSignIn = () => {
+    firebase.auth()
+      .signInWithPopup(ghProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log('GitHub user: ',user);
+        setUser(user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential);
       });
   }
 
@@ -171,6 +180,8 @@ function App() {
       }
       <br />
       <button onClick={handleFbSignIn}>Sign in with facebook</button>
+      <br />
+      <button onClick={handleGithubSignIn}>Sign in with GitHub</button>
 
       {
         user.isSignedIn && <div>
